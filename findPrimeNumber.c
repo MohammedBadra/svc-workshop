75fdda39be0119b4596fbf7fcfa3dbfc4debc8a6
MohBadra <mouni.21android@gmail.com>
2018-01-27 17:17:21 +0200

    the code itself

--- findPrimeNumber.c
+++ findPrimeNumber.c
@@ -1,3 +1,26 @@
+/* this program factorizes prime numbers
+prime factoring */
 
-*/this program factorizes prime numbers
-prime factoring /*
+#include <stdio.h>
+int isPrimeNumber(int number);
+
+int main ()
+{
+    isPrimeNumber(3);
+}
+int isPrimeNumber(int number)
+{
+    int isPrimenumber =1;
+
+    for(int divider = 1; divider <= number/2; divider++ )
+    {
+        if(number%divider ==0)
+        {
+            isPrimeNumber=0;
+        }
+    }
+    isPrimeNumber ? printf("%d is prime number \n", number) : printf("%d is prime number \n", number);
+    return isPrimenumber;
+}
+
+
